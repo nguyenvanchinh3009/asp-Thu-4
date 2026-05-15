3bbd0111fd0ae80fb45c08bac2d37d6324cb21fe
nguyenvanchinh3009 <vanchinh0368@gmail.com>
2026-05-15 15:08:14 +0700

    first commit

---
+++ CMS.Data/Entities/Category.cs
@@ -0,0 +1,21 @@
+﻿//sinhvien : Nguyễn Văn Chính
+//Mssv : 2123110543  
+//Lớp : CCQ2311F    
+//Ngày Tạo:15/05
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    internal class Category
+    {
+    }
+}
