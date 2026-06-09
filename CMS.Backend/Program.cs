fc51e1ede76e591f0eadb48b45b8729d394a4881
nguyenvanliem345 <nguyenvanliem309@gmail.com>
2026-06-09 23:07:03 +0700

    Nop bai 5, 6, 7

---
+++ CMS.Backend/Program.cs
@@ -0,0 +1,78 @@
+﻿using CMS.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// MVC
+builder.Services.AddControllersWithViews();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// =========================
+// CORS
+// =========================
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+// Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Authentication
+// Authentication (Sửa phần này)
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+        // Fix lỗi bảo mật Cookie trên localhost
+        options.Cookie.SameSite = SameSiteMode.Lax;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    });
+
+var app = builder.Build();
+
+// Swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// =========================
+// CORS
+// =========================
+app.UseCors("AllowAll");
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ CMS.Data/Entities/Post.cs
@@ -0,0 +1,28 @@
+﻿//Sinh viên : Nguyễn Văn Chính      
+//MSSV:2123110543   
+//Lớp:CCQ2311F
+//Ngày tạo: 15/05/2026
+//Mô tả: Quản lí bài viết
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+
+
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty; // Nên khởi tạo rỗng để tránh lỗi
+        public string? Content { get; set; } // Thêm dấu ? ở đây
+        public string? ImageUrl { get; set; } // Nên thêm ? cho cả ảnh
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public int CategoryId { get; set; }
+        public virtual Category? Category { get; set; }
+    }
+}
+
